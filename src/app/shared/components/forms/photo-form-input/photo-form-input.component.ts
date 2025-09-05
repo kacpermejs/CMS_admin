4a1f2d681c8dc2fc82430048aa62461813c71152
kacpermejs <kacpermejs@gmail.com>
2025-09-05 15:05:39 +0200

    Layout fix

--- src/app/shared/components/forms/photo-form-input/photo-form-input.component.ts
+++ src/app/shared/components/forms/photo-form-input/photo-form-input.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FORM_INPUT_BASE_VIEW_PROVIDERS } from '../form-input-base/FORM_INPUT_BASE_VIEW_PROVIDERS';
 import { SimpleFormInputBase } from '../form-input-base/SimpleFormInputBase';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ImageUploadComponent } from "../../image-upload/image-upload.component";
 
 @Component({
   selector: 'app-photo-form-input',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, ImageUploadComponent],
   templateUrl: './photo-form-input.component.html',
   styleUrl: './photo-form-input.component.css',
     viewProviders: [...FORM_INPUT_BASE_VIEW_PROVIDERS],
