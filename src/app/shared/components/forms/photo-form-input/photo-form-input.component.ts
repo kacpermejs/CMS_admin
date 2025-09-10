ccdd4ac94fc1ebdd7723ad5a67de5cb40ef000e5
kacpermejs <kacpermejs@gmail.com>
2025-09-10 21:29:06 +0200

    Image input overhaul

--- src/app/shared/components/forms/photo-form-input/photo-form-input.component.ts
+++ src/app/shared/components/forms/photo-form-input/photo-form-input.component.ts
@@ -11,10 +11,18 @@ import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { updateEditableField } from 'app/features/content-entries/store/entry-creation.actions';
 import { selectValues } from 'app/features/content-entries/store/entry-creation.feature';
+import { Menu } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-photo-form-input',
-  imports: [CommonModule, ReactiveFormsModule, ButtonDirective, RouterModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonDirective,
+    RouterModule,
+    Menu
+],
   templateUrl: './photo-form-input.component.html',
   styleUrl: './photo-form-input.component.css',
   viewProviders: [...FORM_INPUT_BASE_VIEW_PROVIDERS],
@@ -29,16 +37,31 @@ export class PhotoFormInputComponent
   modalService = inject(ModalService);
   mediaUploadService = inject(MediaUploadService);
 
-  previewUrl$?: Observable<string | null>
+  menuItems: MenuItem[] = [];
 
-  ngOnInit() {    
+  previewUrl$?: Observable<string | null>;
+
+  ngOnInit() {
     this.previewUrl$ = this.store.select(selectValues).pipe(
-      map(fields => fields[this.controlKey] ?? null),
-      switchMap(fileId => {
+      map((fields) => fields[this.controlKey] ?? null),
+      switchMap((fileId) => {
         if (!fileId) return of(null);
         return this.mediaUploadService.getPreviewUrl(fileId);
       })
     );
+
+    this.menuItems = [
+      {
+        label: 'New',
+        icon: 'pi pi-plus',
+        command: () => this.openUploadModal(),
+      },
+      {
+        label: 'Existing',
+        icon: 'pi pi-folder-open',
+        command: () => this.openSelectionModal(),
+      },
+    ];
   }
 
   get control() {
@@ -46,32 +69,53 @@ export class PhotoFormInputComponent
   }
 
   openUploadModal() {
-    this.modalService.open('upload-image', {
-      queryParams: {
-        mediaForEntryField: this.controlKey
-      },
-      queryParamsHandling: 'merge'
-    }).then( (result) => {      
-      const uploadedFiles: { fileId: string, forControlKey: string }[] = result.uploadedFiles;
-      
-      const fileId = uploadedFiles.find(f => f.forControlKey === this.controlKey)?.fileId;
+    this.modalService
+      .open('upload-image', {
+        queryParams: {
+          mediaForEntryField: this.controlKey,
+        },
+        queryParamsHandling: 'merge',
+      })
+      .then((result) => {
+        const uploadedFiles: { fileId: string; forControlKey: string }[] =
+          result.uploadedFiles;
 
-      this.store.dispatch(updateEditableField({fieldId: this.controlKey, value: fileId}));
-    });
+        const fileId = uploadedFiles.find(
+          (f) => f.forControlKey === this.controlKey
+        )?.fileId;
+
+        this.store.dispatch(
+          updateEditableField({ fieldId: this.controlKey, value: fileId })
+        );
+      });
   }
 
   openSelectionModal() {
-    this.modalService.open('select-existing-image', {
-      queryParams: {
-        mediaForEntryField: this.controlKey
-      },
-      queryParamsHandling: 'merge'
-    }).then( (result) => {      
-      const selectedFiles: { fileId: string, forControlKey: string }[] = result.selectedFiles;
-      
-      const fileId = selectedFiles.find(f => f.forControlKey === this.controlKey)?.fileId;
+    this.modalService
+      .open('select-existing-image', {
+        queryParams: {
+          mediaForEntryField: this.controlKey,
+        },
+        queryParamsHandling: 'merge',
+      })
+      .then((result) => {
+        const selectedFiles: { fileId: string; forControlKey: string }[] =
+          result.selectedFiles;
 
-      this.store.dispatch(updateEditableField({fieldId: this.controlKey, value: fileId}));
-    });
+        const fileId = selectedFiles.find(
+          (f) => f.forControlKey === this.controlKey
+        )?.fileId;
+
+        this.store.dispatch(
+          updateEditableField({ fieldId: this.controlKey, value: fileId })
+        );
+      });
+  }
+
+  getDisplayValue(path: string | null): string {
+    if (!path) return '';
+    // Split into segments and remove first 2
+    const parts = path.split('/');
+    return parts.slice(2).join('/');
   }
 }
